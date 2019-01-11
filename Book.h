af9fc2992353a53a2b07a98f68f58c92ddb0b03f
Marco <marco.a.romero0303@gmail.com>
2019-01-11 01:37:05 -0800

    Create Book.h

---
+++ Book.h
@@ -0,0 +1,14 @@
+#ifndef BOOK_H_INCLUDED
+#define BOOK_H_INCLUDED
+
+class Book
+{
+public:
+    // number of books
+    int numOfBooks;
+    // title and author are self explanatory
+    char title[40];
+    char author[40];
+};
+
+#endif // BOOK_H_INCLUDED
