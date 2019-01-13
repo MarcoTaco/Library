181567d208cc10dc2c7e2e754534de341e2cef94
Marco <marco.a.romero0303@gmail.com>
2019-01-13 01:36:18 -0800

    Update Library.h

--- Library.h
+++ Library.h
@@ -5,13 +5,13 @@
 
 class Library
 {
-private:
+public:
     // number of books in the library
     int bookInLibary;
     // the inventory in the library
     Book inventory[50];
-
-public:
+    // constructor
+    library();
     // this will insert a book
     void insertBook(char bookTitle[], char bookAuthor[], int counting);
     // this will delete a book
@@ -20,14 +20,39 @@ public:
     Book *searchBook(char bookName[]);
 };
 
-void Library::insertBook(char bookTitle[], char bookAuthor[], int counting)
+Library::library()
 {
-
+    // initializing bookInLibrary
+    bookInLibrary = 0;
 }
 
+// thought process was to copy whatever I put in as an insert.
+void Library::insertBook(char bookTitle[], char bookAuthor[], int counting)
+{
+    // copying the book title
+    strcopy(inventory[bookInLibrary].title, bookTitle);
+    //copying the book author
+    strcopy(inventory[bookInLibrary].author, bookAuthor);
+    
+    inventory[bookInLibrary].numOfBooks = counting;
+    
+    cout << "Thanks for inserting a book in the library." << endl;
+    
+    // adding the book to the library
+    ++bookInLibrary;
+}
+
+// thought process was to make a for loop to loop through the array and delete each letter.
 void Library::deleteBook(char bookTitle[])
 {
-
+    for(int i = 0; i < bookInLibrary; i++)
+    {
+        delete inventory[i].title;
+    }
+    cout << "Thanks for deleting the book from the library." << endl;
+    
+    //deleting the book from the library
+    --bookInLibrary;
 }
 
 Book Library::*searchBook(char bookName[])
