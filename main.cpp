e79c24805eecb50d3f2ece53143e1f6209e82a58
Marco <marco.a.romero0303@gmail.com>
2019-01-15 23:58:43 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 // you will be able to insert, delete, or search for a book in a library.
 
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <stdlib.h>
 
 #include "Book.h"
 #include "Library.h"
@@ -11,22 +12,45 @@ using namespace std;
 
 int main()
 {
+    // declaring the class so I can use it in the main
+    Library libr;
+
     char choice;
+    char nameOfBook[40], writer[40], countOfCopies[10];
 
     cout << "Would you like to search, insert, or delete a book (S/I/D)?" << endl;
     cin >> choice;
 
-    if(choice == 'S')
-    {
-        cout << "Search a book name: " << endl;
-    }
-    else if(choice == 'I')
+    if(choice == 'I')
     {
         cout << "Insert a book name: " << endl;
+        cin.getline(nameOfBook, 40);
+        cin.getline(writer, 40);
+        // atoi converts strings to integers
+        libr.insertBook(nameOfBook, writer, atoi(countOfCopies));
     }
     else if(choice == 'D')
     {
         cout << "Delete a book: " << endl;
+        cin.getline(nameOfBook, 40);
+        libr.deleteBook(nameOfBook);
+    }
+    else if(choice == 'S')
+    {
+        cout << "Search for a book: " << endl;
+        cin.getline(nameOfBook, 40);
+
+        Book *books;
+        books = libr.searchBook(nameOfBook);
+        if(books != NULL)
+        {
+            cout << "Book found. " << books->title << endl;
+            cout << books->author << endl;
+        }
+        else
+        {
+            cout << "Book not found." << endl;
+        }
     }
 
     return 0;
